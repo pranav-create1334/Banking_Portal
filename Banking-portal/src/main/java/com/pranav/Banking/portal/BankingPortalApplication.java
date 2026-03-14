8167d4ca7b32577f288682a4f852882fdb647632
Pranav <pranavkusalkar39@gmail.com>
2026-03-14 23:55:43 +0530

    Initial commit - Banking portal project~

---
+++ Banking-portal/src/main/java/com/pranav/Banking/portal/BankingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.Banking.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPortalApplication.class, args);
+	}
+
+}
